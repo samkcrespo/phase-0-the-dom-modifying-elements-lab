f666ce7644eb113d787d20f74e442a699bec60af
Samantha Crespo <samkcrespo@gmail.com>
2022-04-08 13:53:19 -0700

    commit tree 1a5e17ecdacf8bc1b46cda731e4aeafe777c2532

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+
+const element = document.getElementById('main');
+
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Sam is the champion!' 
+
+
 // Write your code here!
